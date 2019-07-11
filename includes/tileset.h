1fcb6ef578e716584dd7cc4be108af421dc2de99
Jean-Paul BELLA <jean.paul.bella@hotmail.fr>
2019-07-11 22:56:04 +0200

    Added fixed window

---
+++ includes/tileset.h
@@ -0,0 +1,8 @@
+//
+// Created by Jean-Paul BELLA on 2019-07-11.
+//
+
+#ifndef GAMEBOYTEST_TILESET_H
+#define GAMEBOYTEST_TILESET_H
+
+#endif //GAMEBOYTEST_TILESET_H
---
+++ res/map/blankScreen.c
@@ -0,0 +1,4 @@
+//
+// Created by Jean-Paul BELLA on 2019-07-10.
+//
+
